9d5c0afb3e18292667504a440d27e77fcd3f461f
jameslawler <services@jameslawler.com>
2022-02-02 21:29:23 +0100

    Use objects for faster performance

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 const isValidSudoku = (board) => {
-  const horizontalSets = [];
-  const verticalSets = [];
-  const smallGridSets = {};
+  const horizontal = {};
+  const vertical = {};
+  const smallGrids = {};
 
   for (let i = 0; i < 9; i++) {
-    horizontalSets.push(new Set());
-    verticalSets.push(new Set());
-
     for (let j = 0; j < 9; j++) {
-      const horizontalValue = board[i][j];
-      if (horizontalValue !== ".") {
-        if (horizontalSets[i].has(horizontalValue)) {
-          return false;
-        }
-        horizontalSets[i].add(horizontalValue);
-      }
+      const value = board[i][j];
 
-      const verticalValue = board[j][i];
-      if (verticalValue !== ".") {
-        if (verticalSets[i].has(verticalValue)) {
-          return false;
-        }
-        verticalSets[i].add(verticalValue);
+      if (value === ".") {
+        continue;
       }
 
       const x = Math.ceil((i + 1) / 3);
       const y = Math.ceil((j + 1) / 3);
+      const gridPosition = `${x}-${y}`;
 
-      if (horizontalValue !== ".") {
-        if (!smallGridSets[`${x}-${y}`]) {
-          smallGridSets[`${x}-${y}`] = new Set();
-        }
-
-        if (smallGridSets[`${x}-${y}`].has(horizontalValue)) {
-          return false;
-        }
-
-        smallGridSets[`${x}-${y}`].add(horizontalValue);
+      if (!horizontal[i]) {
+        horizontal[i] = {};
       }
+
+      if (!vertical[j]) {
+        vertical[j] = {};
+      }
+
+      if (!smallGrids[gridPosition]) {
+        smallGrids[gridPosition] = {};
+      }
+
+      if (
+        horizontal[i][value] ||
+        vertical[j][value] ||
+        smallGrids[gridPosition][value]
+      ) {
+        return false;
+      }
+
+      horizontal[i][value] = true;
+      vertical[j][value] = true;
+      smallGrids[gridPosition][value] = true;
     }
   }
 
